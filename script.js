3a838e85af24b8740aaa81da0135c85d3b4a6a5e
HalParadis <halparadis@gmail.com>
2023-03-19 18:50:47 -0700

    fully functional again after 1st refactor

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const playAgainButton = document.getElementById('play-again-button');
 const chooseNumPlayersContainer = document.getElementById('choose-num-players-container');
 const namePlayersContainer = document.getElementById('name-players-container');
 
-let game; 
+let game;
 
 buildInitialState();
 renderInitialState();
@@ -23,7 +23,9 @@ function buildInitialState() {
         playerHasWon: '',
         finalMoveRendered: false,
         isActualGame: true,
-    
+        needToAddPiece: false,
+        columnChosen: undefined,
+
         gridState: [],
         player1: {
             name: '',
@@ -58,33 +60,30 @@ function buildInitialState() {
     }
 }
 
-function resetState() {
-
+function addPieceByCoor(row, column, player) {
+    addToConnectedPieceSets(game.gridState[row][column], player);
+    checkAndAddUnconnected(player);
+    findAndPushPairs(row, column, player);
+    game.gridState[row][column].contains = 'background-' + game[player].color;
+    game[player].allPlacedPieces.push(game.gridState[row][column]);
+    // maybe change when this function is called
+    updateWinState(player);
+    if (player === 'player1') {
+        game.isPlayer1Turn = false;
+    }
+    else {
+        game.isPlayer1Turn = true;
+    }
 }
 
 function addPiece(column) {
     for (let row = 5; row >= 0; row--) {
         if (!game.gridState[row][column].contains) {
             if (game.isPlayer1Turn) {
-                addToConnectedPieceSets(game.gridState[row][column], 'player1');    
-                checkAndAddUnconnected('player1');
-                findAndPushPairs(row, column, 'player1');      
-                game.gridState[row][column].contains = 'background-' + game.player1.color;
-                game.player1.allPlacedPieces.push(game.gridState[row][column]);
-                game.isPlayer1Turn = false;
-                updateWinState('player1');
-                if (game.numPlayers === 1 && game.isActualGame && !(game.playerHasWon || game.isDraw)) {
-                    compTakesTurn();
-                }
+                addPieceByCoor(row, column, 'player1');
             }
-            else {      
-                addToConnectedPieceSets(game.gridState[row][column], 'player2');
-                checkAndAddUnconnected('player2');
-                findAndPushPairs(row, column, 'player2');           
-                game.gridState[row][column].contains = 'background-' + game.player2.color;
-                game.player2.allPlacedPieces.push(game.gridState[row][column]);
-                game.isPlayer1Turn = true;
-                updateWinState('player2');
+            else {
+                addPieceByCoor(row, column, 'player2');
             }
             return true;
         }
@@ -93,37 +92,10 @@ function addPiece(column) {
 }
 
 function compTakesTurn() {
-    // const actualGame = JSON.parse(JSON.stringify(game));
-    // const moveWeights = [0, 0, 0, 0, 0, 0, 0];
-    // for (let i = 0; i < 3; i++) {
-    //     moveWeights = checkNextTurnWinLose(moveWeights);
-    //     addPiece(chooseBestMove(moveWeights));
-    //     for (let j = 0; j < 7; j++) {
-
-    //     }
-    // }
-    // game = actualGame;
-
-    // let moveWeights = [0, 0, 0, 0, 0, 0, 0];
-    // moveWeights = checkNextTurnWinLose(moveWeights);
-    // let hasAddedPiece = false;
-    // while (!hasAddedPiece) {
-    //     if (addPiece(chooseBestMove(moveWeights))) {
-    //         hasAddedPiece = true;
-    //         renderState();
-    //         game.finalMoveRendered = game.playerHasWon || game.isDraw;
-    //     }
-    // }
-
-    // let moveWeights = [0, 0, 0, 0, 0, 0, 0];
-    // moveWeights = checkNextTurnWinLose(moveWeights);
-    // console.log('moveWeights:');
-    // console.log(moveWeights);
-
     const column = Math.floor(Math.random() * 7);
-    if (addPiece(column)) {
-        renderState();
-        game.finalMoveRendered = game.playerHasWon || game.isDraw;
+    if (!game.gridState[0][column].contains) {
+        game.columnChosen = column;
+        game.needToAddPiece = true;
     }
     else {
         compTakesTurn();
@@ -144,26 +116,30 @@ function chooseBestMove(moveWeights) {
 
 function checkNextTurnWinLose(moveWeights) {
     let actualGame = JSON.parse(JSON.stringify(game));
+
     // debugger;
-    game.isActualGame = false;
     for (let i = 0; i < 7; i++) {
+        game.isActualGame = false;
+        // if (game.isActualGame) {
+        //     debugger;
+        // }
         addPiece(i);
-        if (game.playerHasWon = 'player2') {
+        if (game.playerHasWon === 'player2') {
             moveWeights[i] += 100;
         }
-        else {
-            let testGame = JSON.parse(JSON.stringify(game));
-            for (let j = 0; j < 7; j++) {
-                addPiece(j);
-                if (game.playerHasWon = 'player1') {
-                    moveWeights[i] -= 100;
-                }
-                game = testGame;
-            }
-        }
+        // else {
+        //     let testGame = JSON.parse(JSON.stringify(game));
+        //     for (let j = 0; j < 7; j++) {
+        //         addPiece(j);
+        //         if (game.playerHasWon = 'player1') {
+        //             moveWeights[i] -= 100;
+        //         }
+        //         game = testGame;
+        //     }
+        // }
         game = actualGame;
     }
-    debugger;
+    // debugger;
     return moveWeights;
 }
 
@@ -177,7 +153,7 @@ function findAndPushPairs(row, column, player) {
     }
     else {
         game[player].notConnectedPieces.push(game.gridState[row][column]);
-    } 
+    }
 }
 
 function addToConnectedPieceSets(piece, player) {
@@ -208,7 +184,7 @@ function isInSetOfPieces(newPiece, setOfPieces) {
 
 
 function updateWinState(player) {
-    const numPiecesOnGrid = game.player1.allPlacedPieces.length + game.player1.allPlacedPieces.length;
+    const numPiecesOnGrid = game.player1.allPlacedPieces.length * 2;
     game[player].connectedPieceSets.forEach(set => {
         if (set.length >= 4) {
             game.playerHasWon = player;
@@ -227,8 +203,8 @@ function isInLineWith(newPiece, connectedPieces) {
 
     connectedPieces.forEach(prevPiece => {
         const newDiffs = differenceBetween(newPiece, prevPiece);
-        if ((newDiffs[0] === prevDiffs[0] && newDiffs[1] === prevDiffs[1]) || 
-        (newDiffs[0] === -1 * prevDiffs[0] && newDiffs[1] === -1 * prevDiffs[1])) {
+        if ((newDiffs[0] === prevDiffs[0] && newDiffs[1] === prevDiffs[1]) ||
+            (newDiffs[0] === -1 * prevDiffs[0] && newDiffs[1] === -1 * prevDiffs[1])) {
             isInLine = true;
         }
     });
@@ -257,87 +233,24 @@ function thesePiecesAreTouching(piece1, piece2) {
     return Math.abs(differences[0]) <= 1 && Math.abs(differences[1]) <= 1;
 }
 
+function updateState() {
+    if (!game) {
+        buildInitialState();
+        renderInitialState();
+    }
+    else if (game.needToAddPiece) {
+        if (addPiece(game.columnChosen)) {
+            game.needToAddPiece = false;
+            game.columnChosen = undefined;
+        }
+    }
+    // else if (game.numPlayers === 1 && game.isActualGame 
+    //     && !(game.playerHasWon || game.isDraw) && !isPlayer1Turn) {
+    //     compTakesTurn();
+    // }
+}
+
 //  **************************RENDER FUNCTIONS*************************************
-
-// render
-function renderState() {
-    if (game.numPlayers && !game.numThatHaveInputName) {
-        namePlayersContainer.classList.replace('display-none', 'display-block');
-        chooseNumPlayersContainer.classList.replace('display-flex', 'display-none');
-        if (game.numPlayers === 1) {
-            setName(namePlayersContainer.lastElementChild, game.player2.name, 'color-yellow');
-        }
-    }
-    else if (!game.allNamesFilled) {
-        const playerNameSubContainers = namePlayersContainer.children;
-        if (game.player1.name && !playerNameSubContainers[0].getElementsByTagName('span')[0]) {
-            setName(playerNameSubContainers[0], game.player1.name, 'color-red');
-        }
-        if (game.player2.name && !playerNameSubContainers[1].getElementsByTagName('span')[0]) {
-            setName(playerNameSubContainers[1], game.player2.name, 'color-yellow');
-        }
-        if (game.numThatHaveInputName === game.numPlayers) {
-            setArrowRowColor();
-            game.allNamesFilled = true;
-            // this is probably not the right place for this, I don't think it should be in this function 
-            // should probably go in onBoardClick
-            if (game.numPlayers === 1 && !game.isPlayer1Turn) {
-                compTakesTurn();
-            }
-        }
-    }
-    else if (!game.finalMoveRendered) {
-        setArrowRowColor();
-        for (let row = 0; row < 6; row++) {
-            let rowEl = gameGridEl.children[row];
-            for (let column = 0; column < 7; column++) {
-                let gridSpace = rowEl.children[column];
-                if (game.gridState[row][column].contains) {
-                    gridSpace.classList.replace('background-color-white', game.gridState[row][column].contains);
-                }
-            }
-        }
-    }
-    
-    if (game.playerHasWon || game.isDraw) {
-        // debugger;
-        showWinDraw();
-    }
-}
-
-// create/replace html elements
-function renderInitialState() {
-    // empty grid and arrow row elements
-    gameGridEl.replaceChildren();
-    arrowRowEl.replaceChildren();
-
-    // if there is a game end message remove it
-    const gameEndMessage = document.getElementById('gameEndMessage')
-    if (gameEndMessage) {
-        gameEndMessage.remove();
-    }
-
-    // reset naming elements if possible
-    namePlayersContainer.classList.replace('display-block', 'display-none');
-    chooseNumPlayersContainer.classList.replace('display-none', 'display-flex');
-
-    [...document.getElementsByTagName('span')].forEach(enteredName => enteredName.remove());
-
-    [...document.getElementsByClassName('name-inputs-container')].forEach(nameInputsContainer => nameInputsContainer.classList.replace('display-none', 'display-block'));
-
-    [...document.getElementsByClassName('name-text-input')].forEach(textInput => textInput.value = '');
-
-    // reset play again button if possible
-    playAgainButton.classList.replace('display-block', 'display-none');
-
-    // create arrow row
-    makeArrows();
-
-    // fill in the game grid
-    for (let i = 0; i < 6; i++) {
-        renderRow();
-    }
-}
 
 // adds a row to the game grid html
 function renderRow() {
@@ -424,7 +337,7 @@ function setArrowRowColorTo(newColor, colorToReplace) {
 // removes name inputs, creates new element containing provided name
 function setName(playerNameSubContainer, nameInput, colorClassName) {
     const nameInputsContainer = playerNameSubContainer.getElementsByClassName('name-inputs-container')[0];
-    nameInputsContainer.classList.replace('display-block','display-none');
+    nameInputsContainer.classList.replace('display-block', 'display-none');
     const nameEl = document.createElement('span');
     nameEl.innerText = nameInput;
     nameEl.classList.add(colorClassName);
@@ -432,18 +345,123 @@ function setName(playerNameSubContainer, nameInput, colorClassName) {
     playerNameSubContainer.classList.add('display-flex');
 }
 
+// create/replace html elements
+function renderInitialState() {
+    // empty grid and arrow row elements
+    gameGridEl.replaceChildren();
+    arrowRowEl.replaceChildren();
+
+    // if there is a game end message remove it
+    const gameEndMessage = document.getElementById('gameEndMessage')
+    if (gameEndMessage) {
+        gameEndMessage.remove();
+    }
+
+    // reset naming elements if possible
+    namePlayersContainer.classList.replace('display-block', 'display-none');
+    chooseNumPlayersContainer.classList.replace('display-none', 'display-flex');
+
+    [...document.getElementsByTagName('span')].forEach(enteredName => enteredName.remove());
+
+    [...document.getElementsByClassName('name-inputs-container')].forEach(nameInputsContainer => nameInputsContainer.classList.replace('display-none', 'display-block'));
+
+    [...document.getElementsByClassName('name-text-input')].forEach(textInput => textInput.value = '');
+
+    // reset play again button if possible
+    playAgainButton.classList.replace('display-block', 'display-none');
+
+    // create arrow row
+    makeArrows();
+
+    // fill in the game grid
+    for (let i = 0; i < 6; i++) {
+        renderRow();
+    }
+}
+
+// render
+function renderState() {
+    const playerNameSubContainers = namePlayersContainer.children;
+    const player1NameEl = playerNameSubContainers[0];
+    const player2NameEl = playerNameSubContainers[1];
+    const player1SpanEl = player1NameEl.getElementsByTagName('span')[0];
+    const player2SpanEl = player2NameEl.getElementsByTagName('span')[0];
+
+    if (game.numPlayers && !game.numThatHaveInputName) {
+        namePlayersContainer.classList.replace('display-none', 'display-block');
+        chooseNumPlayersContainer.classList.replace('display-flex', 'display-none');
+        if (game.numPlayers === 1 && !player2SpanEl) {
+            setName(player2NameEl, game.player2.name, 'color-yellow');
+        }
+    }
+    else if (game.numPlayers && !game.allNamesFilled) {
+        if (game.player1.name && !player1SpanEl) {
+            setName(player1NameEl, game.player1.name, 'color-red');
+        }
+        if (game.player2.name && !player2SpanEl) {
+            setName(player2NameEl, game.player2.name, 'color-yellow');
+        }
+        if (game.numThatHaveInputName === game.numPlayers) {
+            setArrowRowColor();
+            game.allNamesFilled = true;
+        }
+    }
+    else if (!game.finalMoveRendered) {
+        setArrowRowColor();
+        for (let row = 0; row < 6; row++) {
+            let rowEl = gameGridEl.children[row];
+            for (let column = 0; column < 7; column++) {
+                let gridSpace = rowEl.children[column];
+                if (game.gridState[row][column].contains) {
+                    gridSpace.classList.replace('background-color-white', game.gridState[row][column].contains);
+                }
+            }
+        }
+    }
+    game.finalMoveRendered = game.playerHasWon || game.isDraw;
+
+    if ((game.playerHasWon || game.isDraw) && game.isActualGame) {
+        showWinDraw();
+    }
+}
+
 //  **************************EVENT HANDLERS*************************************
 
 // maybe a dozen or so helper functions for tiny pieces of the interface
-function onBoardClick() {
+function onBoardClick(event) {
     // update state, maybe with another dozen or so helper functions...
+    const clickedEl = event.target;
+    const parentClassArray = [...clickedEl.parentElement.classList];
+    const classArray = [...clickedEl.classList];
+    if (classArray.includes('num-player-button')) {
+        numPlayersPress(clickedEl);
+    }
+    else if (classArray.includes('name-submit')) {
+        nameSubmit(event);
+    }
+    else if (parentClassArray.includes('arrow-down')) {
+        arrowClicked(event);
+    }
+    else if (clickedEl.id === 'play-again-button') {
+        game = undefined;
+    }
+    // if (!(game.playerHasWon === 'player2' && game.numPlayers === 1)) {
+    //     renderState();
+    //     game.finalMoveRendered = game.playerHasWon || game.isDraw;
+    // } 
+    updateState();
+    renderState();  // show the user the new state
+    // game.finalMoveRendered = game.playerHasWon || game.isDraw;   
 
-    renderState() // show the user the new state
+    if (game.numPlayers === 1 && !game.isPlayer1Turn && game.allNamesFilled && !game.finalMoveRendered) {
+        compTakesTurn();
+        updateState();
+        renderState();
+    }
 }
 
 // called function when number of players is chosen
-function numPlayersPress(event) {
-    const clickedEl = event.target;
+function numPlayersPress(clickedEl) {
     if (clickedEl.tagName === 'BUTTON') {
         if (clickedEl.id === 'one-player-button') {
             game.numPlayers = 1;
@@ -452,47 +470,32 @@ function numPlayersPress(event) {
         else {
             game.numPlayers = 2;
         }
-        renderState();
     }
 }
 
 function nameSubmit(event) {
-    const prevSibling = event.target.previousElementSibling;
-    if (prevSibling) {
-        const nameInput = prevSibling.value;
-        if ([...event.target.classList].includes('name-submit') && nameInput) {
-            if (event.target.parentElement.id === "player1-name-inputs-container") {
-                game.player1.name = nameInput;
-            }
-            else {
-                game.player2.name = nameInput;
-            }
-            game.numThatHaveInputName++;
-            renderState();
+    const nameInput = event.target.previousElementSibling.value;
+    if (nameInput) {
+        if (event.target.parentElement.id === "player1-name-inputs-container") {
+            game.player1.name = nameInput;
         }
+        else {
+            game.player2.name = nameInput;
+        }
+        game.numThatHaveInputName++;
     }
 }
 
 function arrowClicked(event) {
     const column = event.target.dataset.column;
-    if (column !== undefined && game.allNamesFilled && !game.finalMoveRendered) {
-        if (addPiece(column)) {
-            if (!(game.playerHasWon === 'player2' && game.numPlayers === 1)) {
-                renderState();
-                game.finalMoveRendered = game.playerHasWon || game.isDraw;
-            }
-            
-        }
+    if (game.allNamesFilled && !game.finalMoveRendered) {
+        game.needToAddPiece = true;
+        game.columnChosen = column;
     }
 }
 
-function playAgainClicked() {
-    buildInitialState();
-    renderInitialState();
-}
-
 // event listeners
-playAgainButton.addEventListener('click', playAgainClicked);
-chooseNumPlayersContainer.addEventListener('click', numPlayersPress);
-namePlayersContainer.addEventListener('click', nameSubmit);
-arrowRowEl.addEventListener('click', arrowClicked);
+playAgainButton.addEventListener('click', onBoardClick);
+chooseNumPlayersContainer.addEventListener('click', onBoardClick);
+namePlayersContainer.addEventListener('click', onBoardClick);
+arrowRowEl.addEventListener('click', onBoardClick);
